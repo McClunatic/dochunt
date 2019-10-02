28ca2c57ef3ff814977c5b875cdc51963e39d367
Brian McClune <bpmcclune@gmail.com>
2019-10-02 20:35:26 +0000

    added vue config to support running in AWS Cloud9

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+module.exports = {
+  devServer: {
+    compress: true,
+    inline: true,
+    port: 8080,
+    public: '122383a581114da9bc8fab766c8bc512.vfs.cloud9.us-east-2.amazonaws.com',
+  },
+};
