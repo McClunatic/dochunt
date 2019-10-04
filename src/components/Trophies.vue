4dca272388191557131d629b4e9cf697c7f06c9c
Brian McClune <bpmcclune@gmail.com>
2019-10-04 18:10:48 +0000

    made buttons for filters conditionally disabled

--- src/components/Trophies.vue
+++ src/components/Trophies.vue
@@ -18,9 +18,13 @@
                 :placeholder="'Filter by '.concat(field.key)"
               ></b-form-input>
               <b-input-group-append>
-                <b-button variant="reset">
-                  <font-awesome-icon :icon="['fas', 'times']" />
+                <b-button variant="reset" v-if="filters[index]">
+                  <font-awesome-icon :icon="['fas', 'times']"/>
                 </b-button>
+                <b-button variant="reset" v-else disabled>
+                  <font-awesome-icon :icon="['fas', 'times']"/>
+                </b-button>
+
               </b-input-group-append>
             </b-input-group>
           </th>
@@ -69,18 +73,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
