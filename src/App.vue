4b691e6d8aabc000179bf366984252e0808c7295
Brian McClune <bpmcclune@gmail.com>
2019-10-05 02:37:03 +0000

    added Press Start 2P font for jumbotron

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,9 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Press+Start+2P");
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -63,5 +65,8 @@ export default {
   background-image: url("./assets/doc-hunt-logo.svg");
   background-repeat: no-repeat;
   background-position: 99% center;
+}
+.jumbotron > h1 {
+  font-family: "Press Start 2P";
 }
 </style>
