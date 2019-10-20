6d33261b798d1f4328e97d4e036426f76905feee
Brian McClune <bpmcclune@gmail.com>
2019-10-20 13:22:45 +0000

    styled table headers to fix widths

--- src/store.js
+++ src/store.js
@@ -6,10 +6,34 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     fields: [
-      { col: 0, key: "id", label: "Number", sortable: true },
-      { col: 1, key: "title", label: "Title", sortable: true },
-      { col: 2, key: "author", label: "Author", sortable: true },
-      { col: 3, key: "date", label: "Date", sortable: true }
+      {
+        col: 0,
+        key: "id",
+        label: "Number",
+        sortable: true,
+        thStyle: "width: 17%"
+      },
+      {
+        col: 1,
+        key: "title",
+        label: "Title",
+        sortable: true,
+        thStyle: "width: 50%"
+      },
+      {
+        col: 2,
+        key: "author",
+        label: "Author",
+        sortable: true,
+        thStyle: "width: 17%"
+      },
+      {
+        col: 3,
+        key: "date",
+        label: "Date",
+        sortable: true,
+        thStyle: "width: 16%"
+      }
     ],
     kills: []
   },
