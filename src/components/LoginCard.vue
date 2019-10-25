a995b53ad855b4ed2e66862268b58c52a7344557
Brian McClune <bpmcclune@gmail.com>
2019-10-25 17:56:49 +0000

    added form variables and validation to login

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -9,16 +9,32 @@
           <b-input-group-prepend is-text>
             <font-awesome-icon :icon="['fas', 'user']"></font-awesome-icon>
           </b-input-group-prepend>
-          <b-form-input placeholder="Username"></b-form-input>
+          <b-form-input
+            v-model="username"
+            :state="usernameState"
+            v-on:keyup.enter="submit"
+            autofocus
+            placeholder="Username"
+          ></b-form-input>
+          <b-form-invalid-feedback v-html="usernameFeedback">
+          </b-form-invalid-feedback>
         </b-input-group>
         <b-input-group class="p-2">
           <b-input-group-prepend is-text>
             <font-awesome-icon :icon="['fas', 'key']"></font-awesome-icon>
           </b-input-group-prepend>
-          <b-form-input placeholder="Password"></b-form-input>
+          <b-form-input
+            v-model="password"
+            :state="passwordState"
+            v-on:keyup.enter="submit"
+            placeholder="Password"
+            type="password"
+          ></b-form-input>
+          <b-form-invalid-feedback v-html="passwordFeedback">
+          </b-form-invalid-feedback>
         </b-input-group>
         <div class="p-2 mt-2">
-          <b-button block variant="primary">Login</b-button>
+          <b-button block variant="primary" @click="submit">Login</b-button>
         </div>
         <template v-slot:footer>
           Don't have an account? <b-link href="#">Sign Up</b-link>
@@ -30,7 +46,59 @@
 
 <script>
 export default {
-  name: "LoginCard"
+  name: "LoginCard",
+  data: function() {
+    return {
+      username: "",
+      password: "",
+      submitted: false
+    };
+  },
+  watch: {
+    username: function() {
+      this.submitted = false;
+    },
+    password: function() {
+      this.submitted = false;
+    }
+  },
+  computed: {
+    usernameState: function() {
+      return !this.submitted ? null : this.username.length >= 4;
+    },
+    passwordState: function() {
+      return !this.submitted ? null : this.password.length >= 8;
+    },
+    usernameFeedback: function() {
+      if (this.username.length === 0) {
+        return "Please enter your username";
+      } else if (!this.usernameState) {
+        return "Valid usernames have at least 4 characters";
+      } else {
+        return "";
+      }
+    },
+    passwordFeedback: function() {
+      if (this.password.length === 0) {
+        return "Please enter your password";
+      } else if (!this.passwordState) {
+        return "Valid passwords have at least 8 characters";
+      } else {
+        return "";
+      }
+    }
+  },
+  methods: {
+    submit: function() {
+      this.submitted = true;
+      if (this.usernameState && this.passwordState) {
+        console.log("Submitting form...");
+        this.username = "";
+        this.password = "";
+        this.submitted = false;
+      }
+    }
+  }
 };
 </script>
 
