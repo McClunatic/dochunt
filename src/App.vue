3dd23961787f2edcde7e1ffc569ae4f75dce4c2e
Brian McClune <bpmcclune@gmail.com>
2019-10-25 13:47:24 +0000

    adjusted styling of app and home

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Press+Start+2P");
 
+body {
+  height: 100vh;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  height: 100%;
 }
 h1 {
   font-family: "Press Start 2P";
