d6e08c12ada9fda8897bfcf8e3319b656ae986c0
Brian McClune <bpmcclune@gmail.com>
2019-11-02 00:11:07 +0000

    forced transpiling of devtools for edge browser

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,6 @@ module.exports = {
     public: process.env.VUE_APP_PUBLIC
     // public:
     //   "122383a581114da9bc8fab766c8bc512.vfs.cloud9.us-east-2.amazonaws.com"
-  }
+  },
+  transpileDependencies: ['@vue/devtools']
 };
