287b4162b07ae362529a9a9b49f5f8626398613e
Brian McClune <bpmcclune@gmail.com>
2019-11-08 13:25:20 +0000

    linted vue config

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,8 @@ module.exports = {
     // public:
     //   "122383a581114da9bc8fab766c8bc512.vfs.cloud9.us-east-2.amazonaws.com"
   },
-  transpileDependencies: ['@vue/devtools'],
+  transpileDependencies: ["@vue/devtools"],
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: "source-map"
   }
 };
