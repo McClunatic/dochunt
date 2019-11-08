27beedb59b6f0316f5589f9a06edbbf0a3020cd1
Brian McClune <bpmcclune@gmail.com>
2019-11-08 14:23:59 +0000

    added interceptor for 401 status to trigger logout

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@
   </div>
 </template>
 
+<script>
+export default {
+  created: function() {
+    this.$http.interceptors.response.use(undefined, function(err) {
+      if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+        this.$store.dispatch("logout");
+      }
+      Promise.reject(err);
+    });
+  }
+};
+</script>
+
 <style lang="scss">
 body {
   height: 100vh;
